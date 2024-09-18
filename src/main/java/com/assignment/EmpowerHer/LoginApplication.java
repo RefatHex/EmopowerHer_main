24f6de9df0373569a351b32ff87ae2ed9b7cf628
refathex <khondakar00@gmail.com>
2024-09-18 23:02:47 +0600

    FIXING DEPLOYMENT PROBLEM

--- src/main/java/com/assignment/EmpowerHer/LoginApplication.java
+++ src/main/java/com/assignment/EmpowerHer/LoginApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.assignment.EmpowerHer.repository")
 public class LoginApplication {
 
 	public static void main(String[] args) {
